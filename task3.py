33c0e8fba4e11e3c57d24e5ee11ee4e92019c7ef
Alex <chugun777akk@mail.ru>
2022-06-05 18:28:54 +0700

    task 5 is done

--- task3.py
+++ task3.py
@@ -19,11 +19,53 @@ print(f'Least common multiple numbers {number_1} and {number_2} = {LСM}')
 '''
 
 
-
-
 # 2.	Вычислить число Пи c заданной точностью d
 # Пример: при d = 0.001,  c= 3.141.
+
+
 # 3.	Составить список простых множителей натурального числа N
+
+
 # 4.	Дана последовательность чисел. Получить список неповторяющихся элементов исходной последовательности
 # Пример: [1, 2, 3, 5, 1, 5, 3, 10] => [1, 2, 3, 5, 10]
 # 5.  Дан текстовый файл, содержащий целые числа. Удалить из него все четные числа.
+
+import random
+
+
+def make_file_with_numbers(count_of_number):
+    with open('file_number_list.txt', 'w') as data:
+        for i in range(count_of_number):
+            data.write(str(random.randint(-100, 100))+' ')
+
+
+def get_row(str_from_file):
+    with open('file_number_list.txt', 'r') as data:
+        for i in data:
+            str_from_file += i
+    return str_from_file
+
+
+def delete_even_numbers(total_list):
+    in_between_list = []
+    for i in total_list:
+        if int(i) % 2 != 0:
+            in_between_list.append(int(i))
+    return in_between_list
+
+
+def rewrite_in_new_file(total_list):
+    with open('totalfile.txt', 'w') as data:
+        data.write(str(total_list))
+
+
+count_of_numbers = int(input('Enter tht count of number: '))
+make_file_with_numbers(count_of_numbers)
+str_from_file = ''
+str_from_file = get_row(str_from_file)
+# print(str_from_file)
+total_list = str_from_file.split()
+# print(total_list)
+total_list = delete_even_numbers(total_list)
+print(total_list)
+rewrite_in_new_file(total_list)
